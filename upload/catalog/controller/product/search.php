d82a7d73517f2a18cc129ba68a15e276a4ae3dbc
CMS BroCart <artur.coder@brocart.net>
2021-06-10 16:31:13 +0300

    return old method getTotalProducts

--- upload/catalog/controller/product/search.php
+++ upload/catalog/controller/product/search.php
@@ -192,9 +192,9 @@ class ControllerProductSearch extends Controller {
 				'limit'               => $limit
 			);
 
-			$results = $this->model_catalog_product->getProducts($filter_data);
+			$product_total = $this->model_catalog_product->getTotalProducts($filter_data);
 			
-			$product_total = $this->model_catalog_product->getProTotal();
+			$results = $this->model_catalog_product->getProducts($filter_data);
 			
 			if ($product_total && $limit && $page > 1) {
 				if (!isset($this->session->data['maxpage'])) {	
